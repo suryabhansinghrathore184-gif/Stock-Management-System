bb9e5ddf8c3908a5713fe8c0f34b22e10a7c6d6d
suryabhansinghrathore184 <suryabhansinghrathore184@gmail.com>
2026-07-09 23:55:35 +0530

    Bound customer and supplier master pages to load actual profile avatars dynamically from database

--- customer.Master.cs
+++ customer.Master.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web;
 using System.Web.UI;
 
@@ -8,10 +10,42 @@ namespace StockMangementSystem
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // Centralized Customer Authentication Check
             if (Session["cid"] == null)
             {
                 Response.Redirect("login.aspx");
+                return;
+            }
+
+            if (!IsPostBack)
+            {
+                LoadCustomerProfile();
+            }
+        }
+
+        private void LoadCustomerProfile()
+        {
+            try
+            {
+                DataCon dc = new DataCon();
+                string query = "SELECT customerphoto FROM NewCustomer WHERE Id = @Id";
+                SqlParameter[] param = new SqlParameter[]
+                {
+                    new SqlParameter("@Id", SqlDbType.VarChar) { Value = Session["cid"] }
+                };
+                DataSet ds = dc.GetDataSet(query, param);
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    string photo = ds.Tables[0].Rows[0]["customerphoto"].ToString();
+                    ImgProfile.ImageUrl = string.IsNullOrEmpty(photo) ? "https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&w=150&q=80" : "/Links/" + photo;
+                }
+                else
+                {
+                    ImgProfile.ImageUrl = "https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&w=150&q=80";
+                }
+            }
+            catch
+            {
+                ImgProfile.ImageUrl = "https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&w=150&q=80";
             }
         }
     }
--- supplier.Master.cs
+++ supplier.Master.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web;
 using System.Web.UI;
 
@@ -8,10 +10,42 @@ namespace StockMangementSystem
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // Centralized Supplier Authentication Check
             if (Session["sid"] == null)
             {
                 Response.Redirect("login.aspx");
+                return;
+            }
+
+            if (!IsPostBack)
+            {
+                LoadSupplierProfile();
+            }
+        }
+
+        private void LoadSupplierProfile()
+        {
+            try
+            {
+                DataCon dc = new DataCon();
+                string query = "SELECT supplierphoto FROM NewSupplier WHERE supplierid = @Id";
+                SqlParameter[] param = new SqlParameter[]
+                {
+                    new SqlParameter("@Id", SqlDbType.VarChar) { Value = Session["sid"] }
+                };
+                DataSet ds = dc.GetDataSet(query, param);
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    string photo = ds.Tables[0].Rows[0]["supplierphoto"].ToString();
+                    ImgProfile.ImageUrl = string.IsNullOrEmpty(photo) ? "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?auto=format&fit=crop&w=150&q=80" : "/Links/" + photo;
+                }
+                else
+                {
+                    ImgProfile.ImageUrl = "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?auto=format&fit=crop&w=150&q=80";
+                }
+            }
+            catch
+            {
+                ImgProfile.ImageUrl = "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?auto=format&fit=crop&w=150&q=80";
             }
         }
     }
